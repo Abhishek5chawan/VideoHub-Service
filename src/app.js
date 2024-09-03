252c761275cdfe84e41d3d2cd17215893aba1da1
Abhishek5chawan <abhishekchawan907@gmail.com>
2024-09-03 10:48:16 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -9,9 +9,9 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"})) // Set the maximum allowed request body size to 16KB from body, to prevent the server from crashing if a malicious user sends a huge request.
-app.use(express.urlencoded({extended: true, limit: "16kb"})) // Set the maximum allowed request body size to 16KB from the url, to prevent the server 
-app.use(express.static("public")) // Serve static files from the public folder which are not confidential
+app.use(express.json({limit: "16kb"})) 
+app.use(express.urlencoded({extended: true, limit: "16kb"})) 
+app.use(express.static("public")) 
 app.use(cookieParser())
 
 export { app }
