cc9612c820ac9b97aaf0c6fd09a1d0dc46ee3455
Abhishek_Chawan <abhishekchawan907@gmail.com>
2024-09-09 14:07:50 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,14 @@ app.use(express.urlencoded({extended: true, limit: "16kb"})) //parsing url encod
 app.use(express.static("public")) //serves static files which are not sensitive like images
 app.use(cookieParser()) //parses cookies
 
+//routes import 
+
+import userRouter from "./routes/user.routes.js"
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+//example url : http://localhost:8000/api/v1/users/register
+
 export { app }
